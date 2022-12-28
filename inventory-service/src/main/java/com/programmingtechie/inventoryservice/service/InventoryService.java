10e28cba9a85232d94f88cb7536d8831e86792ae
Low Jung Xuan <lowjungxuan@gmail.com>
2022-12-28 18:22:09 +0800

    inventory done

---
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -0,0 +1,17 @@
+package com.programmingtechie.inventoryservice.service;
+
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+    private final InventoryRepository inventoryRepository;
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode){
+//        return true;
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+}
