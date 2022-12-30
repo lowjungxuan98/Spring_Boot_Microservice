d7bbf6f029e13e2251c4c55c29af8ead44aaebe6
Low Jung Xuan <lowjungxuan@gmail.com>
2022-12-30 12:47:04 +0800

    api gateway with keycloak done

--- discovery-server/src/main/java/com/programmingtechie/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/programmingtechie/discoveryserver/config/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebFluxSecurity
 public class SecurityConfig {
     @Value("${eureka.username}")
     private String username;
